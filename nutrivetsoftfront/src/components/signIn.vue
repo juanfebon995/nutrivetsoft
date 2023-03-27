77905007ecfeb3eb6140de45c3e469d7a4ec0405
juanfebon995 <juan.riascos.bonilla@gmail.com>
2023-03-26 23:58:05 -0500

    [NUTRIVETSOFT] -> TEST

--- nutrivetsoftfront/src/components/signIn.vue
+++ nutrivetsoftfront/src/components/signIn.vue
@@ -1,9 +1,6 @@
 <template>
     <form id="app">
-        <label class="label" for="task">Nueva tarea: </label>
-        <input type="text" v-model="newTask" id="task">
-        {{ newTask }}
-        <input type="submit" value="Crear tarea">
+        
     </form>
 </template>
   
