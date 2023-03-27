2b68dfad255778707d2b7d3549a7e397dcf7995f
juanfebon995 <juan.riascos.bonilla@gmail.com>
2023-03-26 23:53:33 -0500

    [NUTRIVETSOFT] -> FRONT VUEJS - CREACIÓN DE DOCUMENTACION - CREACION COMPONENTE SIGN IN

--- nutrivetsoftfront/src/App.vue
+++ nutrivetsoftfront/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <singIn />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import singIn from './components/signIn.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    singIn
   }
 }
 </script>
---
+++ nutrivetsoftfront/src/components/signIn.vue
@@ -0,0 +1,18 @@
+<template>
+    <form id="app">
+        <label class="label" for="task">Nueva tarea: </label>
+        <input type="text" v-model="newTask" id="task">
+        {{ newTask }}
+        <input type="submit" value="Crear tarea">
+    </form>
+</template>
+  
+<script>
+export default {
+    data: () => ({
+        newTask: ""
+    })
+};
+</script>
+  
+<style></style>
